ea90195689e545a7ce1fa5960e5dc05333b7ec8b
SarahDowning <sarahdowningx@gmail.com>
2021-12-13 15:53:52 +0000

    GLA-5 GLA-6 #comment Added read all and create functionality

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 "Use strict"
+
+const gameApplication = "http://localhost:8080"
+const getOutput = document.querySelector("#getOutput");
+
+// Get All games request
+const getAllGames = () => {
+    
+    axios
+        .get(`${gameApplication}/getAll`)
+        .then(res => {
+            console.log(res);
+
+            const games = res.gameData;
+            getOutput.innerHTML = "";
+            for (let game of games) {
+                const gameContainer = document.createElement("div");
+
+                const gameTitle = document.createElement("p");
+                gameTitle.innerText = `Game Title: ${game.gameTitle}`;
+                gameContainer.appendChild(gameTitle);
+
+                const genre = document.createElement("p");
+                genre.innerText = `Genre: ${game.genre}`;
+                gameContainer.appendChild(genre);
+
+                const platform = document.createElement("p");
+                platform.innerText = `Platform: ${game.platform}`;
+                gameContainer.appendChild(platform);
+
+                const publisher = document.createElement("p");
+                publisher.innerText = `Publisher: ${game.publisher}`;
+                gameContainer.appendChild(publisher);
+
+                const releaseYear = document.createElement("p");
+                releaseYear.innerText = `Release Year: ${game.releaseYear}`;
+                gameContainer.appendChild(releaseYear);
+
+                getOutput.appendChild(gameContainer);
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+
+// Post request for create
+document.querySelector("#addGameForm").addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // const form = event.target
+    const form = event.target;
+    const gameData = {
+        gameTitle: form.gameTitle.value,
+        genre: form.genre.value,
+        platform: form.platform.value,
+        publisher: form.publisher.value,
+        releaseYear: form.releaseYear.value
+    };
+
+    console.log(gameData);
+
+    axios
+        .post(`${gameApplication}/create`, gameData)
+        .then((res) => {
+            form.reset();
+            form.gameTitle.focus();
+            console.log(res);
+        }).catch(err => console.error(err));
+});
